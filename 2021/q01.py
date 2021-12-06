a94e5e46124d67af2f969aba21a4d26277d887b8
Ryan Baker (TA-422) <Ryan.Baker@bmwgroup.com>
2021-12-06 15:26:30 +0000

    Day 1/2a

--- 2021/q01.py
+++ 2021/q01.py
@@ -1,10 +1,20 @@
 from aocd import get_data
-from config import AOCD_SESSION
+##from ..util.config import AOCD_SESSION, AOCD_DIR
 
 """
 --- Day 1: Sonar Sweep ---
 https://adventofcode.com/2021/day/1
 """
+
+AOCD_SESSION = ""
+Test_data = """
+"""
+
+
 ns = get_data(session=AOCD_SESSION, day=1, year=2021, block=False)
 
-print(ns)
+T_data = [int(n) for n in ns.split()]
+
+print("part a:", sum(n2 > n1 for n1, n2 in zip(T_data, T_data[1:])))
+print("part b:", sum(n2 > n1 for n1, n2 in zip(T_data, T_data[3:])))
+
--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-import pathlib
-
-# AOCD
-AOCD_DIR = os.environ.get("AOCD_DIR", "<YOUR-AOCD_DIR-KEY>")
-AOCD_SESSION = os.environ.get("AOCD_SESSION", "<YOUR-AOCD_SESSION-LOCATION>")
